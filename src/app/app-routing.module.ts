43059a4a338721f64a5e4fcdf24433c5590c39f4
Razor7w <seba.carroza@gmail.com>
2020-10-15 19:18:46 -0300

    [Clase] 13-10-2020 - Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { Tarea1Component } from './components/tarea1/tarea1.component';
+import { ClaseAComponent } from './components/clase-a/clase-a.component';
+import { RutasComponent } from './components/rutas/rutas.component';
 
 
-const routes: Routes = [];
+
+// const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'tarea1', component: Tarea1Component },
+  { path: 'clase', component: ClaseAComponent },
+  { path: 'claserutas', component: RutasComponent },
+  { path: 'claserutas/:id', component: RutasComponent },
+  { path: '**', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
